4d8f5d516b7f5de9ca484014a26c501a6c550cc0
Dominik Hautau <dominikhautau@gmail.com>
2024-06-05 14:20:42 +0200

    simple toast message example

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,29 @@
-import "@/styles/globals.css";
+import { useState } from "react";
+import Toast from "@/components/Toast";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const [toast, setToast] = useState({
+    isOpen: false,
+    message: "",
+  });
+
+  function handleToast(message) {
+    setToast({ isOpen: true, message });
+  }
+
+  function handleToastClose() {
+    setToast({ isOpen: false, message: "" });
+  }
+
+  return (
+    <>
+      <Component {...pageProps} onToast={handleToast} />
+      <Toast
+        isOpen={toast.isOpen}
+        message={toast.message}
+        onToastClose={handleToastClose}
+        duration={3000}
+      />
+    </>
+  );
 }
